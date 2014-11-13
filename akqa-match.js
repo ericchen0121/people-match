1f76f4bc47f8fb344899e02fb4b30d0975674632
Eric Savvy <eric.chen0121@gmail.com>
2014-11-12 21:57:31 -0800

    persistence in Mongo complete with form

--- akqa-match.js
+++ akqa-match.js
@@ -1,35 +1,39 @@
 People = new Mongo.Collection("people")
 
 if (Meteor.isClient) {
-    // Q: do we put templates in this file?
   // pass data into templates using helpers
   Template.people.helpers({
-    // people: [
-    //   { name: 'Eric Chen', department: 'developer', title: 'associate web dev', client: 'Audi' },
-    //   { name: 'Brandon Bilfield', department: 'developer', title: 'technical delivery manager', client: 'Audi'},
-    //   { name: 'Tyler Burton', department: 'developer', title: 'sr. dev', client: 'Audi' },
-    //   { name: 'Michael Moriarty', department: 'developer', title: 'sr. dev', client: 'Audi' },
-    //   { name: 'Jeff Wesson', department: 'developer', title: 'sr. dev', client: 'Audi' },
-    //   { name: 'Eric Clifford', department: 'developer', title: 'technical architect', client: 'Audi' },
-    //   { name: 'Ryan Brewster', department: 'developer', title: 'sr. dev', client: 'Audi' },
-    //   { name: 'Saj Momin', department: 'developer', title: 'cq guy', client: 'Audi' },
-    //   { name: 'Mohan', department: 'developer', title: 'cq guy', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' }
-    // ]
     people: function() {
       return People.find({})
     }
   });
 
+  Template.add_person_form.events({
+    'submit .new-person': function(event){
+      // Store values via form
+      var name = event.target.name.value;
+      var department = event.target.department.value;
+      var title = event.target.title.value;
+      var client = event.target.client.value;
+
+      // Add to db
+      People.insert({
+        name: name,
+        department: department,
+        title: title,
+        client: client
+      })
+      
+      // Clear form
+      event.target.name.value = ''
+      event.target.department.value = ''
+      event.target.title.value = ''
+      event.target.client.value = ''
+
+      // Prevent default form submit
+      return false
+    }
+  })
 }
 
 if (Meteor.isServer) {
