9c8c3a1b510921497fed99d1b68bd195da73017b
Eric Chen <eric.chen0121@gmail.com>
2014-11-12 18:22:38 -0800

    add mongo

--- akqa-match.js
+++ akqa-match.js
@@ -1,38 +1,35 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+People = new Mongo.Collection("people")
 
-  // Q: do we put templates in this file?
+if (Meteor.isClient) {
+    // Q: do we put templates in this file?
+  // pass data into templates using helpers
   Template.people.helpers({
-    people: [
-      { name: 'Eric Chen', department: 'developer', title: 'associate web dev', client: 'Audi' },
-      { name: 'Brandon Bilfield', department: 'developer', title: 'technical delivery manager', client: 'Audi'},
-      { name: 'Tyler Burton', department: 'developer', title: 'sr. dev', client: 'Audi' },
-      { name: 'Michael Moriarty', department: 'developer', title: 'sr. dev', client: 'Audi' },
-      { name: 'Jeff Wesson', department: 'developer', title: 'sr. dev', client: 'Audi' },
-      { name: 'Eric Clifford', department: 'developer', title: 'technical architect', client: 'Audi' },
-      { name: 'Ryan Brewster', department: 'developer', title: 'sr. dev', client: 'Audi' },
-      { name: 'Saj Momin', department: 'developer', title: 'cq guy', client: 'Audi' },
-      { name: 'Mohan', department: 'developer', title: 'cq guy', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
-      { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' }
-    ]
+    // people: [
+    //   { name: 'Eric Chen', department: 'developer', title: 'associate web dev', client: 'Audi' },
+    //   { name: 'Brandon Bilfield', department: 'developer', title: 'technical delivery manager', client: 'Audi'},
+    //   { name: 'Tyler Burton', department: 'developer', title: 'sr. dev', client: 'Audi' },
+    //   { name: 'Michael Moriarty', department: 'developer', title: 'sr. dev', client: 'Audi' },
+    //   { name: 'Jeff Wesson', department: 'developer', title: 'sr. dev', client: 'Audi' },
+    //   { name: 'Eric Clifford', department: 'developer', title: 'technical architect', client: 'Audi' },
+    //   { name: 'Ryan Brewster', department: 'developer', title: 'sr. dev', client: 'Audi' },
+    //   { name: 'Saj Momin', department: 'developer', title: 'cq guy', client: 'Audi' },
+    //   { name: 'Mohan', department: 'developer', title: 'cq guy', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' },
+    //   { name: 'tbd', department: 'tbd', title: 'tbd', client: 'Audi' }
+    // ]
+    people: function() {
+      return People.find({})
+    }
   });
 
-  // Template.person.events({
-  //   'click button': function () {
-  //     // increment the counter when button is clicked
-  //     Session.set("counter", Session.get("counter") + 1);
-  //   }
-  // });
 }
 
 if (Meteor.isServer) {
